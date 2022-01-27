3c317d7e15a6e25f768739dc2649c860713c64d5
AbdAsh <59181667+AbdAsh@users.noreply.github.com>
2022-01-27 09:48:18 +0300

    Added Timeline

---
+++ components/TimeLine.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <HeadingBlock class="title" :text="'Timeline'" />
+    <img class="image" src="../assets/timeline.svg" />
+  </div>
+</template>
+
+<script>
+import HeadingBlock from "~/../zekoder-web-components/src/components/heading-block/HeadingBlock.vue";
+export default {
+    components: { HeadingBlock }
+}
+</script>
+
+<style scoped lang="scss">
+.container {
+  padding: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 50px;
+  margin-bottom: 50px;
+  .title {
+    font-family: Raleway;
+    font-size: 36px;
+    color: white;
+    margin-bottom: 40px;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -6,6 +6,7 @@
     <Features />
     <EcoSystem />
     <DistributionPlan />
+    <TimeLine />
   </div>
 </template>
 
