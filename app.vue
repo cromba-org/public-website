a1b6c6a5ec1fe8a0ee4459b988fc958b8eda3d01
AbdAsh <59181667+AbdAsh@users.noreply.github.com>
2022-02-08 11:00:39 +0300

    Added document title and favicon changed

--- app.vue
+++ app.vue
@@ -3,3 +3,19 @@
     <NuxtPage />
   </div>
 </template>
+
+<script>
+export default {
+    head: {
+    link: [
+      {
+        rel: 'icon',
+        href: '../assets/logo.svg',
+      },
+    ],
+  },
+  created() {
+    document.title = 'Cromba is an open source social media for the free world'
+  },
+}
+</script>
